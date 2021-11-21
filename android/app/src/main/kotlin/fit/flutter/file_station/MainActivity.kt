117da77cd66cbe711032ba73d5413d6e78c72d53
temp <245646299@qq.com>
2021-11-21 15:01:04 +0800

    [新增]适配安卓手势导航条

--- android/app/src/main/kotlin/fit/flutter/file_station/MainActivity.kt
+++ android/app/src/main/kotlin/fit/flutter/file_station/MainActivity.kt
@@ -1,13 +1,13 @@
 package fit.flutter.dsm_helper
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import android.os.Bundle
+import android.view.WindowManager
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine( flutterEngine:
-                                         FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);   //设置沉浸式虚拟键，在MIUI系统中，虚拟键背景透明。原生系统中，虚拟键背景半透明。
     }
 }
