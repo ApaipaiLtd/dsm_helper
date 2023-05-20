7a3d1a219bbc52b42c36c1d4a57a3016e88176db
temp <245646299@qq.com>
2023-05-20 09:11:12 +0800

    [修复]APP自动更新无法安装

--- lib/pages/update/update.dart
+++ lib/pages/update/update.dart
@@ -3,9 +3,12 @@ import 'package:dsm_helper/themes/app_theme.dart';
 import 'package:dsm_helper/util/function.dart';
 import 'package:dsm_helper/widgets/animation_progress_bar.dart';
 import 'package:dsm_helper/widgets/neu_back_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:neumorphic/neumorphic.dart';
 import 'package:open_file/open_file.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class Update extends StatefulWidget {
   final Map data;
@@ -71,7 +74,95 @@ class _UpdateState extends State<Update> {
 
   install() async {
     //检查安装权限
-    await OpenFile.open(filePath);
+    bool hasPermission = await Permission.requestInstallPackages.request().isGranted;
+    print("install permission: $hasPermission");
+    if (!hasPermission) {
+      print("xxx");
+      showCupertinoModalPopup(
+        context: context,
+        builder: (context) {
+          return Material(
+            color: Colors.transparent,
+            child: NeuCard(
+              width: double.infinity,
+              padding: EdgeInsets.all(22),
+              bevel: 5,
+              curveType: CurveType.emboss,
+              decoration: NeumorphicDecoration(color: Theme.of(context).scaffoldBackgroundColor, borderRadius: BorderRadius.vertical(top: Radius.circular(22))),
+              child: SafeArea(
+                top: false,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "更新失败",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      "权限不足，无法自动安装更新，请使用浏览器下载更新",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      height: 22,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: NeuButton(
+                            onPressed: () async {
+                              Navigator.of(context).pop();
+                              launchUrlString(widget.data['url'], mode: LaunchMode.externalApplication);
+                            },
+                            decoration: NeumorphicDecoration(
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            bevel: 5,
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              "使用浏览器下载",
+                              style: TextStyle(fontSize: 18, color: Colors.redAccent),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Expanded(
+                          child: NeuButton(
+                            onPressed: () async {
+                              Navigator.of(context).pop();
+                            },
+                            decoration: NeumorphicDecoration(
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            bevel: 5,
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              "取消",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    } else {
+      await OpenFile.open(filePath);
+    }
   }
 
   cancel() {
