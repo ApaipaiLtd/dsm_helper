9d8ffb09a985e50be34b90f2b3b46a408f0a8f66
temp <245646299@qq.com>
2023-10-24 14:12:59 +0800

    [新增]虚拟机-集群

--- lib/pages/virtual_machine/enums/guest_status_enums.dart
+++ lib/pages/virtual_machine/enums/guest_status_enums.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 enum GuestStatusEnum {
   running(label: "运行中", color: Color(0xFF25B85F)),
-  shutdown(label: "已关机", color: Color(0xFFFF8D1A)),
+  shutdown(label: "已关机", color: Colors.black54),
+  shutting_down(label: "正在关机", color: Color(0xFFFF8D1A)),
   saved(label: "已暂停", color: Colors.black54),
   none(label: "无", color: Color(0xFFFF8D1A)),
   unknown(label: "未知", color: Colors.black54);
